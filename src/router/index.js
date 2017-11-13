68acb5036c5b869153babd4a11a014d3e931d5a4
toby <toby@skaffold.com>
2017-11-14 06:59:14 +1100

    moved unused files

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,30 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+//import Hello from '@/components/Hello'
 import GameList from '@/components/GameList'
-import UserList from '@/components/UserList'
-import UserDetails from '@/components/UserDetails'
+//import UserList from '@/components/UserList'
+//import UserDetails from '@/components/UserDetails'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-    ,
+    //{
+    //  path: '/',
+    //  name: 'Hello',
+    //  component: Hello
+    //}
+    //,
     //{
     //  path: '/games',
     //  name: 'Games',
     //  component: Games
     //},
-    { name:'games', path: '/games',         component: GameList },
-    { name:'home', path: '/users',         component: UserList },
-    { name:'user_new', path: '/user/new', component: UserDetails, props: { isNewUser: true} },
-    { name:'user_edit', path: '/user/:id', component: UserDetails, props: true }
+    { name:'games', path: '/',         component: GameList },
+    { name:'games', path: '/games',         component: GameList }
+    //{ name:'home', path: '/users',         component: UserList },
+    //{ name:'user_new', path: '/user/new', component: UserDetails, props: { isNewUser: true} },
+    //{ name:'user_edit', path: '/user/:id', component: UserDetails, props: true }
 
   ]
 })
