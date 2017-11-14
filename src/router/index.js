cf9130a188cf6498627107739036d0a614a8c94e
toby <toby@skaffold.com>
2017-11-14 20:48:22 +1100

    just a single route

--- src/router/index.js
+++ src/router/index.js
@@ -20,8 +20,8 @@ export default new Router({
     //  name: 'Games',
     //  component: Games
     //},
-    { name:'games', path: '/',         component: GameList },
-    { name:'games', path: '/games',         component: GameList }
+    { name:'games', path: '/',         component: GameList }
+    //{ name:'games', path: '/games',         component: GameList }
     //{ name:'home', path: '/users',         component: UserList },
     //{ name:'user_new', path: '/user/new', component: UserDetails, props: { isNewUser: true} },
     //{ name:'user_edit', path: '/user/:id', component: UserDetails, props: true }
